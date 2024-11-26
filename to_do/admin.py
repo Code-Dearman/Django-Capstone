4b1485c971acb0afe4e3d12cd90d998e73fcdb4f
Isaac Dearman <isaac.dearman@gmail.com>
2024-11-26 15:23:56 +0000

    finalise admin controls

--- to_do/admin.py
+++ to_do/admin.py
@@ -7,7 +7,7 @@ from .models import To_Do_List
 class ToDoAdmin(SummernoteModelAdmin):
     list_display = ('list_title', 'slug', 'status', 'approved')
     search_fields = ['list_title']
-    list_filter = ('status',)
+    list_filter = ('status', 'approved', 'created_on',)
     prepopulated_fields = {'slug': ('list_title',)}
     summernote_fields = ('list_content',)
 
