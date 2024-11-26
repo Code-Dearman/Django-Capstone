883106d3b82436e11d5728e62a0f9a4a48e5678a
Isaac Dearman <isaac.dearman@gmail.com>
2024-11-26 13:55:05 +0000

    add to_do_list model

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "Private"), (1, "Public"))
+
+class To_Do_List(models.Model):
+
+    """
+    Stores a single to do list related to :model:`auth.User`.
+    """
+
+    list_title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="to_do_list")
+    list_content = TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    approved = models.BooleanField(default=False)
