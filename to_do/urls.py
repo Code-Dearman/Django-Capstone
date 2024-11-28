0212d28ae0b235b92cc97a44e722e378d454aca7
Isaac Dearman <isaac.dearman@gmail.com>
2024-11-28 16:36:21 +0000

    add profile view and tempalte

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,7 +1,9 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 
 urlpatterns = [
     path('', views.ToDoListView.as_view(), name='home'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
--- to_do/views.py
+++ to_do/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import To_Do_List
 
 # Create your views here.
@@ -13,3 +14,20 @@ class ToDoListView(generic.ListView):
     queryset = To_Do_List.objects.filter(status=1, approved=True).order_by("-created_on")
     template_name = 'to_do/to_do_list.html'
     paginate_by = 6
+
+
+class ProfileView(LoginRequiredMixin, generic.ListView):
+    """
+    Displays all personal to do lists created by a user.
+    """
+    model = To_Do_List
+    template_name = 'to_do/profile.html'
+    context_object_name = 'user_lists'
+
+    def get_queryset(self):
+        """
+        Returns only lists which match the current logged in user.
+        """
+        return To_Do_List.objects.filter(author=self.request.user)
+    
+
