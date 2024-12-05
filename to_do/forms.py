82e5fa298b867f905be6798ffe39cf2e70f77c49
Isaac Dearman <isaac.dearman@gmail.com>
2024-12-05 10:56:55 +0000

    add task completion to form and template

--- to_do/forms.py
+++ to_do/forms.py
@@ -6,7 +6,7 @@ from .models import To_Do_List
 class ToDoListForm(forms.ModelForm):
     class Meta:
         model = To_Do_List
-        fields = ['list_title', 'list_content', 'status']
+        fields = ['list_title', 'list_content', 'status', 'complete',]
         widgets = {
             'list_content': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Write your to-do list here...'}),
         }
